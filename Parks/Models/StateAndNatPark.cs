7521650dcec87d318c6cfed9a0f20ae4f05cfcca
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-30 15:06:19 -0700

    add model class, properties defined in ParksApi, also added new GetStateAndNatPark() method

---
+++ Parks/Models/StateAndNatPark.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace Parks.Models
+{
+  public class StateAndNatPark
+  {
+    public int StateAndNatParkId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string LocationByState { get; set; }
+
+    public static List<StateAndNatPark> GetStateAndNatParks()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<StateAndNatPark> stateAndNatParkList = JsonConvert.DeserializeObject<List<StateAndNatPark>>(jsonResponse.ToString());
+
+      return stateAndNatParkList;
+    }
+  }
+}
